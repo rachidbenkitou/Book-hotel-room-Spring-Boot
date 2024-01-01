68e3a61c20cdbe017eea59a6a80a3cc34ac7ed92
Rachid Benkitou <rachidbenkitou2021@gmail.com>
2024-01-01 17:25:10 +0100

    create a bean to solve cors origin issue

---
+++ src/main/java/com/benkitou/hotel/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.benkitou.hotel.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200") // Replace with your Angular app's URL
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("Origin", "Content-Type", "Accept", "Authorization")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
